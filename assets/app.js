9fdfb5a8db02c5b69ee6de6222473181ead56460
Ben Fung <benfung1996@gmail.com>
2020-03-08 14:53:40 -0700

    added time format and math function
    created math function and append new row to html DOM.

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-$function() {
+$(function() {
 
     // Initailize Firebase
     var config = {
@@ -23,7 +23,7 @@ $function() {
         // var to store user input
         var trainName = $("#train-name-input").val().trim();
         var destination = $("#destination-input").val().trim();
-        var firstTrain = $("#time-input").val().trim();
+        var firstTrain = moment($("#time-input").val().trim(), "HH:mm");
         var frequency = $("frequency-input").val().trim();
         
         // create local var to store train data as attr
@@ -67,8 +67,39 @@ $function() {
         console.log(FirstTrainTime);
         console.log(Frequency);
 
-        
-    })
+        //set time format
+        var nextArrival = moment.unix(NextArrival).format("HH:mm");
+        var frequency = moment.unix(Frequency).format("mm");
+        var minAway = moment.unix(MinAway).format("mm");
+
+        // calculate next arrival, minutes away from current time.
+        var firstTrainConverted = moment(FirstTrainTime, "HH:mm").subtract("1, days");
+        console.log(firstTrainConverted);
+
+        var difference = moment().diff(moment(firstTrainConverted), "minutes");
+        console.log(difference);
+
+        var remainder = difference % frequency;
+        console.log("remainder: " + remainder);
+
+        var MinAway = frequency - remainder;
+        console.log("minutes away: " + MinAway);
+
+        var NextArrival = moment().add(MinAway, "minutes").format("HH:mm");
+        console.log("next arrival: " + NextArrival);
+
+        // create new row 
+        var newRow = $("<tr>".append(
+            $("<td>").text(TrainName),
+            $("<td>").text(Destination),
+            $("<td>").text(Frequency),
+            $("<td>").text(NextArrival),
+            $("<td>").text(MinAway)
+        ));
+
+        // append new row to html DOM
+        $("#train-table").append(newRow);
+    });
 
 
-}
+});
